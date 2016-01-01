69b1aad84c30bc9be7221d4fb0da88f957fcea4f
shdowofdeath <elhayefrat@gmail.com>
2016-01-01 03:57:45 -0800

    Update fixing_devstack_dep.sh

--- fixing_devstack_dep.sh
+++ fixing_devstack_dep.sh
@@ -1,7 +1,14 @@
 ehco "This is a rhel/centos machine ? or unbutu machine ? , case unbutu press u / centos/redhat pleasee c"
-read 
+read os
 
+if [ $os == "c" ]; then
+sudo su
+yum install python-setuptools
+easy_install pip && exit
+./stack.sh
+else 
 sudo su
 apt-get install python-setuptools
 easy_install pip && exit
 ./stack.sh
+fi
