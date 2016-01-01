c064dcec36380f6a69ec93dc90c8aecf5150be1d
shdowofdeath <elhayefrat@gmail.com>
2016-01-01 03:54:55 -0800

    Create fixing_devstack_dep.sh

---
+++ fixing_devstack_dep.sh
@@ -0,0 +1,7 @@
+ehco "This is a rhel/centos machine ? or unbutu machine ? , case unbutu press u / centos/redhat pleasee c"
+read 
+
+sudo su
+apt-get install python-setuptools
+easy_install pip && exit
+./stack.sh
